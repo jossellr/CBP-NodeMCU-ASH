312a34688ead188fe63eac8e8fc74d0656497f58
José Luis López Ruiz <llopez@ujaen.es>
2024-01-09 00:19:45 +0100

    Create Ejercicio_15.ino

---
+++ Ejercicio_15/Ejercicio_15.ino
@@ -0,0 +1,21 @@
+void setup() 
+{
+    Serial.begin(9600);
+    
+    // Inicialización.
+    pinMode(SENSOR_PIN, INPUT);
+    digitalWrite(SENSOR_PIN, LOW);
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW); 
+}
+ 
+void loop() 
+{
+    // Si encuentra un obstáculo...
+    if (digitalRead(SENSOR_PIN) == LOW) {
+        digitalWrite(LED_PIN, HIGH); 
+        Serial.print("Obstacle detected!");
+    }
+    else
+        digitalWrite(LED_PIN, LOW);
+}
